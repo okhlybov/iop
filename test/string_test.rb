61bdb37d6e35ada990be8ccc6142e5d94debc8c9
Oleg A. Khlybov <fougas@mail.ru>
2019-02-20 16:25:00 +0500

    Added basic string test.

---
+++ test/string_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+require 'iop/string'
+
+class StringTest < Test::Unit::TestCase
+
+  include IOP
+
+  def test_small
+    t = '123456789'
+    (1..10).each do |i|
+      (StringSplitter.new(t, i) | (s = StringMerger.new)).process!
+      assert_equal t, s.to_s
+    end
+  end
+
+end
