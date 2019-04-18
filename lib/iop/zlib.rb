97a4a4aeba9e1a3adb9699f4eea75c0fd63ab3f0
Oleg A. Khlybov <fougas@mail.ru>
2019-04-18 16:00:06 +0500

    Doc updates.

--- lib/iop/zlib.rb
+++ lib/iop/zlib.rb
@@ -5,11 +5,29 @@ require 'zlib'
 module IOP
 
 
+  #
+  # Filter class to perform data compression with Zlib algorithm.
+  #
+  # This class is an adapter for the standard Ruby +Zlib::Deflate+ class.
+  #
+  # Note that this class does not produce valid _.gz_ files - use {GzipCompressor} for this purpose.
+  #
+  # ### Use case: compress a string.
+  #
+  #     require 'iop/zlib'
+  #     require 'iop/string'
+  #     ( IOP::StringSplitter.new('Hello IOP') | IOP::ZlibCompressor.new | (s = IOP::StringMerger.new) ).process!
+  #     puts s.to_s
+  #
+  # @since 0.1
+  #
   class ZlibCompressor
 
     include Feed
     include Sink
 
+    # Creates class instance.
+    # @param args [Array] arguments passed to +Zlib::Deflate+ constructor
     def initialize(*args)
       @args = args
     end
@@ -34,11 +52,30 @@ module IOP
   end
 
 
+  #
+  # Filter class to perform data decompression with Zlib algorithm.
+  #
+  # This class is an adapter for the standard Ruby +Zlib::Inflate+ class.
+  #
+  # Note that this class can not decompress _.gz_ files - use {GzipDecompressor} for this purpose.
+  #
+  # ### Use case: decompress a Zlib-compressed part of a file skipping a header and compute MD5 hash sum of the uncompressed data.
+  #
+  #     require 'iop/zlib'
+  #     require 'iop/file'
+  #     require 'iop/digest'
+  #     ( IOP::FileReader.new('input.dat', offset: 16) | IOP::ZlibDecompressor.new | (d = IOP::DigestComputer.new(Digest::MD5.new)) ).process!
+  #     puts d.digest.hexdigest
+  #
+  # @since 0.1
+  #
   class ZlibDecompressor
 
     include Feed
     include Sink
 
+    # Creates class instance.
+    # @param args [Array] arguments passed to +Zlib::Inflate+ constructor
     def initialize(*args)
       @args = args
     end
@@ -63,11 +100,29 @@ module IOP
   end
 
 
+  #
+  # Filter class to perform Gzip data compression.
+  #
+  # This class is an adapter for the standard Ruby +Zlib::GzipWriter+ class.
+  #
+  # This class produces valid _.gz_ files.
+  #
+  # ### Use case: compress a string and store it to .gz file.
+  #
+  #     require 'iop/zlib'
+  #     require 'iop/file'
+  #     require 'iop/string'
+  #     ( IOP::StringSplitter.new('Hello IOP') | IOP::GzipCompressor.new | IOP::FileWriter.new('hello.gz') ).process!
+  #
+  # @since 0.1
+  #
   class GzipCompressor
 
     include Feed
     include Sink
 
+    # Creates class instance.
+    # @param args [Array] arguments passed to +Zlib::GzipWriter+ constructor
     def initialize(*args)
       @args = args
     end
@@ -94,6 +149,23 @@ module IOP
   end
 
 
+  #
+  # Filter class to perform Gzip data compression.
+  #
+  # This class is an adapter for the standard Ruby +Zlib::GzipWriter+ class.
+  #
+  # This class can decompress _.gz_ files.
+  #
+  # ### Use case: decompress a .gz file and compute MD5 hash sum of uncompressed data.
+  #
+  #     require 'iop/zlib'
+  #     require 'iop/file'
+  #     require 'iop/digest'
+  #     ( IOP::FileReader.new('hello.gz') | IOP::GzipDecompressor.new | (d = IOP::DigestComputer.new(Digest::MD5.new)) ).process!
+  #     puts d.digest.hexdigest
+  #
+  # @since 0.1
+  #
   class GzipDecompressor < ZlibDecompressor
     def initialize
       super(16)
